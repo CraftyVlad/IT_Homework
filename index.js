0f0797687d0e9abe1d90895fb33dcf211055e8d8
Vladsem77 <vladsemboss@gmail.com>
2023-10-11 19:00:11 +0300

    complete version

---
+++ index.js
@@ -0,0 +1,17 @@
+const SCROLL_SPECS = document.querySelector(".SpecsScroll");
+const SCROLL_GAMES = document.querySelector(".GamesScroll");
+const SCROLL_XBOX = document.querySelector(".XboxScroll");
+const SCROLL_SUPPORT = document.querySelector(".SupportScroll");
+
+SCROLL_XBOX.addEventListener("click", (e) => {
+	scrollTo(0, 1);
+});
+SCROLL_SPECS.addEventListener("click", (e) => {
+	scrollTo(0, 800);
+});
+SCROLL_GAMES.addEventListener("click", (e) => {
+	scrollTo(0, 2100);
+});
+SCROLL_SUPPORT.addEventListener("click", (e) => {
+	scrollTo(0, 9999);
+});

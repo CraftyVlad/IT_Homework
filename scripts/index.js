246d206646bc4c9aa4ab835957bb7dfe004399bd
Vladsem77 <vladsemboss@gmail.com>
2023-10-30 16:25:12 +0200

    made media query

--- scripts/index.js
+++ scripts/index.js
@@ -20,6 +20,4 @@ SCROLL_SUPPORT.addEventListener("click", (e) => {
 const BURGER_BTN = document.querySelector(".burger");
 BURGER_BTN.addEventListener("click", () => {
 	BURGER_BTN.classList.toggle("active");
-	const MOB_MENU = document.querySelector(".mobile_menu");
-	MOB_MENU.classList.toggle("active");
 });

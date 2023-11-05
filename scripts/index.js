cd2aa33a9cdfcc3bdd0da16dcfe1afcc6cff0a35
Vladsem77 <vladsemboss@gmail.com>
2023-11-05 13:48:01 +0200

    updated index, support, js and css

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,3 @@
-const SCROLL_XBOX = document.querySelector(".header__Xbox_scroll");
-const SCROLL_SPECS = document.querySelector(".header__Specs_scroll");
-const SCROLL_GAMES = document.querySelector(".header__Games_scroll");
-const SCROLL_SUPPORT = document.querySelector(".header__Support_scroll");
-
-SCROLL_XBOX.addEventListener("click", (e) => {
-	scrollTo(0, 1);
-});
-SCROLL_SPECS.addEventListener("click", (e) => {
-	scrollTo(0, 1050);
-});
-SCROLL_GAMES.addEventListener("click", (e) => {
-	scrollTo(0, 2350);
-});
-SCROLL_SUPPORT.addEventListener("click", (e) => {
-	scrollTo(0, 9999);
-});
-
 const BURGER_BTN = document.querySelector(".burger");
 BURGER_BTN.addEventListener("click", () => {
 	BURGER_BTN.classList.toggle("active");
